33abe44ffb1ed3b3742258755c4e627c192ed5d1
x7enia <x7enia@gmail.com>
2025-01-30 14:14:53 +0100

    Fix undefined kPrimaryColor

--- lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Custom color palette
-const Color kColor1 = Color(0xFFF5EFFF); // Background
-const Color kColor2 = Color(0xFFE5D9F2); // Larger elements
-const Color kColor3 = Color(0xFFCDC1FF); // Smaller elements
-const Color kColor4 = Color(0xFFA294F9); // Accent color
